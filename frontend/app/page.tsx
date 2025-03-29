d92ae8eebdae50a05f6f9946918031c0ab37d7be
phil-not-funny <schrenk.philip@hotmail.com>
2025-03-29 12:42:39 +0100

    fixed bug

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1 className="text-primary text-6xl font-bold">Welcome to Triptales</h1>
+      <h1 className="text-primary text-6xl font-bold">Welcome to Triptales</h1> 
     </div>
   );
 }
