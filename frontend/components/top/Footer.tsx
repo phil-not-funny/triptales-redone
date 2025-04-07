6fc4b9a3c0bab40512343cf1a81ec28cb918473b
Philip Schrenk <schrenk.philip@hotmail.com>
2025-04-07 13:52:25 +0200

    updated foooter

--- frontend/components/top/Footer.tsx
+++ frontend/components/top/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
@@ -60,8 +61,9 @@ const Footer: React.FC = () => {
             </p>
             {/* Social Icons */}
             <div className="flex gap-3">
-              <a
-                href="#"
+              <Link
+                href="https://github.com/phil-not-funny/triptales-redone"
+                target="_blank"
                 className="hover:text-primary-hover text-gray-500 transition-colors duration-200"
               >
                 <svg
@@ -69,21 +71,9 @@ const Footer: React.FC = () => {
                   fill="currentColor"
                   viewBox="0 0 24 24"
                 >
-                  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+                  <path d="M12 2C6.475 2 2 6.475 2 12c0 4.42 2.865 8.164 6.839 9.489.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.156-1.11-1.462-1.11-1.462-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.833.092-.647.35-1.088.636-1.338-2.22-.252-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12c0-5.525-4.475-10-10-10z" />
                 </svg>
-              </a>
-              <a
-                href="#"
-                className="hover:text-primary-hover text-gray-500 transition-colors duration-200"
-              >
-                <svg
-                  className="h-5 w-5"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
-                </svg>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
