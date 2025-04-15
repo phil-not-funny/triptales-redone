ca26a62351ead71a11216a141294a943f381429c
phil-not-funny <schrenk.philip@hotmail.com>
2025-04-15 15:33:53 +0200

    fixed faker errors

--- backend/TripTales.Webapi/Infrastructure/TripTalesContext.cs
+++ backend/TripTales.Webapi/Infrastructure/TripTalesContext.cs
@@ -81,33 +81,47 @@ namespace Triptales.Webapi.Infrastructure
             Users.AddRange(users);
             SaveChanges();
 
-            var posts = new Faker<Post>(locale: "de").CustomInstantiator(f =>
-            {
-                f.Lorem.Locale = "de";
-                var date = f.Date.Between(DateTime.UtcNow.AddDays(-20), DateTime.UtcNow.AddDays(-1));
-                var date2 = f.Date.Between(date, DateTime.UtcNow);
-
-                var numberOfDays = f.Random.Int(1, 5);
-                var days = new List<Post.Day>();
-
-                for (int i = 0; i < numberOfDays; i++)
+            var posts = new Faker<Post>(locale: "de")
+                .CustomInstantiator(f =>
                 {
-                    var totalDaysDifference = (date2 - date).Days;
-                    var dayOffset = i * (totalDaysDifference / Math.Max(1, numberOfDays - 1));
-                    var dayDate = date.AddDays(dayOffset);
+                    f.Lorem.Locale = "de";
 
-                    var day = new Post.Day(
-                        title: f.Lorem.Sentence(5, 2),
-                        description: f.Lorem.Sentence(25, 4),
-                        date: DateOnly.FromDateTime(dayDate)
+                    var startDateTime = f.Date.Between(DateTime.UtcNow.AddDays(-20), DateTime.UtcNow.AddDays(-1));
+                    var startDate = DateOnly.FromDateTime(startDateTime);
+                    var endDateTime = f.Date.Between(startDateTime, DateTime.UtcNow);
+                    var endDate = DateOnly.FromDateTime(endDateTime);
+
+                    var numberOfDays = f.Random.Int(1, 5);
+                    var days = new List<Post.Day>();
+
+                    for (int i = 0; i < numberOfDays; i++)
+                    {
+                        var totalDaysDifference = endDate.DayNumber - startDate.DayNumber;
+                        var dayOffset = i * (totalDaysDifference / Math.Max(1, numberOfDays - 1));
+                        var dayDate = startDate.AddDays(dayOffset);
+
+                        var day = new Post.Day(
+                            title: f.Lorem.Sentence(5, 2),
+                            description: f.Lorem.Sentence(25, 4),
+                            date: dayDate
+                        );
+
+                        days.Add(day);
+                    }
+
+                    var post = new Post(
+                        title: f.Lorem.Sentence(7, 2),
+                        description: f.Lorem.Sentence(35, 5),
+                        author: f.PickRandom(users),
+                        startDate: startDate,
+                        endDate: endDate,
+                        days: days
                     );
 
-                    days.Add(day);
-                }
-
-                var post = new Post(f.Lorem.Sentence(7, 2), f.Lorem.Sentence(35, 5), f.PickRandom(users), date, date2, days);
-                return post;
-            }).Generate(10).ToList();
+                    return post;
+                })
+                .Generate(10)
+                .ToList();
             Posts.AddRange(posts);
             SaveChanges();
         }
